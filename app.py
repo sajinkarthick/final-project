507dfbfbb0e2bba7156748f23a35652126e8d060
sajinkarthick <144696825+sajinkarthick@users.noreply.github.com>
2025-05-20 11:02:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ uploaded_file = st.file_uploader("Upload a face image", type=["jpg", "jpeg", "pn
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     label, probs = predict_emotion(image)
 
